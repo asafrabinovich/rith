a8dbff4d089f4c58ecc0f76b17904a131493b634
Yarin <yarin10061@gmail.com>
2020-09-05 18:14:07 +0300

    no sticky footer

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -3,27 +3,18 @@ import React, {Component} from "react";
 
 const Footer = () =>{
     const style = {
-        color:"white",
         borderTop: "1px solid #E7E7E7",
         textAlign: "center",
-        position: "fixed",
-        left: "0",
-        bottom: "0",
         height: "60px",
         width: "100%",
         marginTop: "60px"
     };
 
-    const position = {
-        display: 'block',
-        height: '60px',
-        width: '100%',
-      }
     return(
 
-        <footer style={position} className="page-footer font-small">
+        <footer className="page-footer font-small">
 
-            <div style={style} class="footer-copyright text-center bg-dark py-3">© 2020 Copyright:
+            <div style={style} class="footer-copyright text-center bg-light py-3">© 2020 Copyright:
                 <a href="http://localhost:3000/home"> WeRent</a>
             </div>
 
